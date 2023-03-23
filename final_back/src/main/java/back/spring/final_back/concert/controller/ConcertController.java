9f7ad8391e738c8a2b460dcce7d4f556a86156cb
melll93 <yemeltu@naver.com>
2023-03-24 01:08:59 +0900

    concerToday 추가(CURDATE로 해당 날짜에 맞는 데이터 출력

--- final_back/src/main/java/back/spring/final_back/concert/controller/ConcertController.java
+++ final_back/src/main/java/back/spring/final_back/concert/controller/ConcertController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:3333")
+@CrossOrigin("http://localhost:3333/")
 @Controller
 @ResponseBody
 @RequiredArgsConstructor
