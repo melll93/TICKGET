00ed48178db9f1e92d28e91122aafd12fad588f4
kojonghoon <dui7272@gmail.com>
2023-03-26 22:37:08 +0900

    TogetherPage

--- final_front/src/pages/community/TogetherPage.jsx
+++ final_front/src/pages/community/TogetherPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import Table from "react-bootstrap/Table";
 
 const TogetherPage = () => {
   return (
@@ -8,7 +9,36 @@ const TogetherPage = () => {
       <Sidebar />
       <div className="center">
         <Header />
-        Together
+        <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>게시글1</td>
+                        <td>artistJay</td>
+                        <td>2022-03-19</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>게시글2</td>
+                        <td>artistJay</td>
+                        <td>2022-03-19</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>게시글2</td>
+                        <td>artistJay</td>
+                        <td>2022-03-19</td>
+                    </tr>
+                </tbody>
+            </Table>
       </div>
     </>
   );
