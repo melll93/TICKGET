f21eb0e8f78ecce0378fc810681d118ddf427991
kojonghoon <dui7272@gmail.com>
2023-03-28 17:31:23 +0900

    feat. method modify

--- final_back/src/main/java/back/spring/final_back/board/controller/BoardController.java
+++ final_back/src/main/java/back/spring/final_back/board/controller/BoardController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
