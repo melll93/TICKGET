39734e231b5eafc83114c0b228f65bc54b6a341a
kojonghoon <dui7272@gmail.com>
2023-03-29 21:22:50 +0900

    feat.board

--- final_front/src/pages/board/BoardList.jsx
+++ final_front/src/pages/board/BoardList.jsx
@@ -1,32 +1,32 @@
-import { Alert } from "react-bootstrap";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import { useNavigate } from "react-router-dom";
+import { jsonboardListDB } from "../../axios/board/boardLogic";
 
 const BoardList = () => {
-    console.log('BoardList')
-    const navigate = useNavigate()
+  console.log("BoardList");
+  const [deptList, setDeptList] = useState([]);
+  const navigate = useNavigate();
 
-
-    //전체조회
-    const boardList=()=>{
-
+  //전체조회(Json으로)
+  const jsonBoardList = async () => {
+    const res = await jsonboardListDB({ deptno: 0 });
+    console.log(res.data);
+    if (res.data) {
+      setDeptList(res.data);
+    } else {
+      console.log("부서목록 조회 실패");
     }
+  };
 
-    //글작성하기
-    const boardWrite=()=>{
-      navigate("write")
-    }
+  //글 수정하기
+  const boardUpdate = () => {};
 
-    //글 수정하기
-    const boardUpdate=()=>{
-
-    }
-
-    //글삭제하기
-    const boardDelete=()=>{
-
-    }
+  //글삭제하기
+  const boardDelete = () => {};
+  
+  
   return (
     <div>
       <Table striped bordered hover>
@@ -38,6 +38,7 @@ const BoardList = () => {
             <th style={{ textAlign: "center", width: "250px" }}>작성일</th>
           </tr>
         </thead>
+        {}
         <tbody>
           <tr>
             <td style={{ textAlign: "center" }}>1</td>
@@ -45,27 +46,27 @@ const BoardList = () => {
             <td style={{ textAlign: "center" }}>artistJay</td>
             <td style={{ textAlign: "center" }}>2023-03-26</td>
           </tr>
-          <tr>
-            <td style={{ textAlign: "center" }}>2</td>
-            <td>게시글2</td>
-            <td style={{ textAlign: "center" }}>artistJay</td>
-            <td style={{ textAlign: "center" }}>2023-03-26</td>
-          </tr>
-          <tr>
-            <td style={{ textAlign: "center" }}>3</td>
-            <td>게시글2</td>
-            <td style={{ textAlign: "center" }}>artistJay</td>
-            <td style={{ textAlign: "center" }}>2023-03-26</td>
-          </tr>
         </tbody>
+        {/* dmalsmfal;mfa; */}
       </Table>
-      <Button style={{ backgroundColor: "black", color:"white" }} onClick={boardList}>전체조회</Button>
+      <Button
+        style={{ backgroundColor: "black", color: "white" }}
+        onClick={jsonBoardList}
+      >
+        전체조회
+      </Button>
       &nbsp;
-      <Button style={{ backgroundColor: "black" }}onClick={boardWrite}>글 작성하기</Button>
+      <Button style={{ backgroundColor: "black" }} onClick={()=>navigate("/together/write")}>
+        글 작성하기
+      </Button>
       &nbsp;
-      <Button style={{ backgroundColor: "black" }} onClick={boardUpdate}>글 수정하기</Button>
+      <Button style={{ backgroundColor: "black" }} onClick={boardUpdate}>
+        글 수정하기
+      </Button>
       &nbsp;
-      <Button style={{ backgroundColor: "black" }} onClick={boardDelete}>글 삭제하기</Button>
+      <Button style={{ backgroundColor: "black" }} onClick={boardDelete}>
+        글 삭제하기
+      </Button>
     </div>
   );
 };
