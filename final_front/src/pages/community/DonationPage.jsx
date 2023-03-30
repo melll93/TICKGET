3ac4009f2135a261a7c84dc4b721704f116ea237
hun0711 <sh6351@naver.com>
2023-03-30 19:57:46 +0900

    도네이션페이지작업

--- final_front/src/pages/community/DonationPage.jsx
+++ final_front/src/pages/community/DonationPage.jsx
@@ -1,66 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
-import SitePick from "../../components/MainPage/SitePick";
-import WeeksHot from "../../components/MainPage/WeeksHot";
-import { getOpenSoonListDB } from "../../axios/main/main";
-import OpenSoon from "../../components/MainPage/OpenSoon";
-
-const DonationPage = () => {
-  const [openSoonList, setOpenSoonList] = useState([])
-  /*   const [openSoonList, setOpenSoonList] = useState([])
-  
-    const getOpenSoonList = () => {
-      getOpenSoonListDB().then(setOpenSoonList);
-    };
-  
-    useEffect(() => {
-      getOpenSoonList();
-    }, []);
-  
-  
-  useEffect(() => {
-    getOpenSoonList();
-  }, []);
-     */
-
-  return (
-    <>
-      <Sidebar />
-      <div className="center">
-        <Header />
-        Donation - 메인홈페이지에 추가할 코드 테스트 중 (성훈)
-        <Tabs
-          defaultActiveKey="openSoon"
-          id="justify-tab-example"
-          className="tab3"
-          justify
-        >
-          <Tab eventKey="sitePick" title="사이트's PICK!">
-            <SitePick />
-          </Tab>
-          <Tab eventKey="openSoon" title="오픈 예정">
-          </Tab>
-          <Tab eventKey="weeksHot" title="이 주의 공연">
-            <WeeksHot />
-          </Tab>
-        </Tabs>
-        defaultActiveKey="openSoon"
-        id="justify-tab-example"
-        className="tab3"
-        justify
-        <Tab eventKey="sitePick" title="사이트's PICK!">
-          <SitePick />
-        </Tab>
-        <Tab eventKey="openSoon" title="오픈 예정">
-          <OpenSoon openSoonList={openSoonList} />
-        </Tab>
-        <Tab eventKey="weeksHot" title="이 주의 공연">
-          <WeeksHot />
-        </Tab>
-      </div >
-    </>
-  );
 import { Button, Table } from 'react-bootstrap'
 import DonationFooter from "../donation/DonationFooter";
 import Footer from "../../components/Footer";
