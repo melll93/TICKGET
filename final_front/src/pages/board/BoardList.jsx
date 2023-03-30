5dbfd439137127538a54effd13a39c9f2946b358
kojonghoon <dui7272@gmail.com>
2023-03-30 19:19:51 +0900

    feat.board

--- final_front/src/pages/board/BoardList.jsx
+++ final_front/src/pages/board/BoardList.jsx
@@ -1,32 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import { useNavigate } from "react-router-dom";
 import { jsonboardListDB } from "../../axios/board/boardLogic";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const BoardList = () => {
   console.log("BoardList");
-  const [deptList, setDeptList] = useState([]);
+  const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    jsonBoardList();
+  }, []);
 
   //전체조회(Json으로)
   const jsonBoardList = async () => {
-    const res = await jsonboardListDB({ deptno: 0 });
+    const res = await jsonboardListDB();
     console.log(res.data);
     if (res.data) {
-      setDeptList(res.data);
+      setBoardList(res.data);
     } else {
       console.log("부서목록 조회 실패");
     }
   };
-
+  
   //글 수정하기
   const boardUpdate = () => {};
 
   //글삭제하기
   const boardDelete = () => {};
-  
-  
+
   return (
     <div>
       <Table striped bordered hover>
@@ -38,16 +42,16 @@ const BoardList = () => {
             <th style={{ textAlign: "center", width: "250px" }}>작성일</th>
           </tr>
         </thead>
-        {}
         <tbody>
-          <tr>
-            <td style={{ textAlign: "center" }}>1</td>
-            <td>게시글1</td>
-            <td style={{ textAlign: "center" }}>artistJay</td>
-            <td style={{ textAlign: "center" }}>2023-03-26</td>
-          </tr>
+          {boardList.map((board) => (
+            <tr key={board.board_no}>
+              <td style={{ textAlign: "center" }}>{board.board_no}</td>
+              <td>{board.board_title}</td>
+              <td style={{ textAlign: "center" }}>{board.member_id}</td>
+              <td style={{ textAlign: "center" }}>{board.board_date}</td>
+            </tr>
+          ))}
         </tbody>
-        {/* dmalsmfal;mfa; */}
       </Table>
       <Button
         style={{ backgroundColor: "black", color: "white" }}
@@ -56,7 +60,10 @@ const BoardList = () => {
         전체조회
       </Button>
       &nbsp;
-      <Button style={{ backgroundColor: "black" }} onClick={()=>navigate("/together/write")}>
+      <Button
+        style={{ backgroundColor: "black" }}
+        onClick={() => navigate("/together/write")}
+      >
         글 작성하기
       </Button>
       &nbsp;
