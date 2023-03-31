34cbe269f8400b9e23c119b126034619155d6b1a
YeoEunGyeong <yeg0420@gmail.com>
2023-04-01 01:05:32 +0900

    signup login 0331

--- final_front/src/pages/member/LoginPage.jsx
+++ final_front/src/pages/member/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import NaverLogin from "../../api/login/NaverLogin";
@@ -8,7 +8,20 @@ const LoginPage = ({ user, setUser }) => {
   const CALLBACK_URL = "http://localhost:3333/oauth/login/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${CALLBACK_URL}&response_type=code`;
   // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=6a3741ce2b33e194c3e799c86fdc2cb2&redirect_uri=${CALLBACK_URL}&response_type=code`;
-
+  // 로그인 필요 정보
+  const [id, setId] = useState()
+  const [password, setPassword] = useState()
+  
+  //
+  const handleIdChange = (event) => {
+    setId(event.target.value);
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
   return (
     <>
       <Sidebar />
@@ -16,12 +29,12 @@ const LoginPage = ({ user, setUser }) => {
         <div className="login">
           {/********************** 자체 회원 로그인 **********************/}
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicId">
+            <Form.Group className="mb-3" onChange={handleIdChange} controlId="formBasicId">
               <Form.Label>ID</Form.Label>
               <Form.Control type="id" placeholder="ID를 입력해주세요." />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" onChange={handlePasswordChange} controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
