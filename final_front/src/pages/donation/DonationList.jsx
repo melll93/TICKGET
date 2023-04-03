0e7fc5ecde8b1cc0cfe77c45cd1cef0265924f1e
melll <yemeltu@naver.com>
2023-04-03 10:50:44 +0900

    donation fix

--- final_front/src/pages/donation/DonationList.jsx
+++ final_front/src/pages/donation/DonationList.jsx
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { don_boardListDB } from '../../axios/donation/donationLogic'
-import DonationRow from './DonationRow'
+import React, { useEffect, useState } from "react";
+import { don_boardListDB } from "../../axios/donation/donationLogic";
+import DonationRow from "./DonationRow";
 
 const DonationList = () => {
+  const [board, setBoard] = useState([
+    {
+      cb_gubun: "qna_title",
+      keyword: "PT10회권양도합니다.",
+    },
+  ]);
 
-  const [board,setBoard] = useState({
-    cb_gubun:'qna_title',
-    keyword:'PT10회권양도합니다.'
-    })
+  const [boards, setBoards] = useState([]);
 
-  const [boards,setBoards] = useState([{
-  }])
-  
+  // don_boardListDB의 return은 Promise이므로 then으로 다시 데이터를 처리해줘야함.
   useEffect(() => {
-    don_boardListDB().then(setBoards);
+    don_boardListDB().then((res) => {
+      setBoards(res.data); // 응답 Promise로부터 data를 꺼내 boards에 세팅.
+    });
   }, []);
-  
-
 
   return (
     <>
-        {boards.map(boards => (
-              <DonationRow key={boards.don_bno} boards={boards}/>
-            ))} 
+      {boards.map((boards) => (
+        <DonationRow key={boards.donBno} boards={boards} />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default DonationList
+export default DonationList;
