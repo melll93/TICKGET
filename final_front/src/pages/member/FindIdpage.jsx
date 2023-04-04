c900e4ff27cc14a03087a95c0c5cd885729987a3
YeoEunGyeong <yeg0420@gmail.com>
2023-04-05 00:42:14 +0900

    signUp login0405

--- final_front/src/pages/member/FindIdpage.jsx
+++ final_front/src/pages/member/FindIdpage.jsx
@@ -21,14 +21,14 @@ const FindIdPage = () => {
   }
   return (
     <LoginForm>
-    <MyH1>이메일 찾기</MyH1>
+    <MyH1>아이디 찾기</MyH1>
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center',Content: 'center', marginTop: '20px', width:"100%"}}>
       <MyLabel> 이름
-        <MyInput type="text" id="name" placeholder="이름을 입력해주세요." 
+        <MyInput type="text" id="name" placeholder="회원님의 이름을 입력해주세요." 
         onChange={(e)=>{changeMemInfo(e);}}/>
       </MyLabel>
       <MyLabel> 전화번호
-        <MyInput type="number" id="hp" placeholder="전화번호를 입력해주세요." 
+        <MyInput type="number" id="hp" placeholder="회원님의 전화번호를 입력해주세요." 
         onChange={(e)=>{changeMemInfo(e);}} />
       </MyLabel>
       <BButton onClick={()=>{find();}}>찾기</BButton>
--- final_front/src/util/authLogic.js
+++ final_front/src/util/authLogic.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, EmailAuthProvider } from "firebase/auth";
 
 class AuthLogic {
   constructor() {
@@ -72,6 +72,51 @@ export const loginGoogle = (auth, googleProvider) => {
         resolve(user);
       })
       .catch((e) => reject(e));
+  });
+};
+
+export const linkEmail = (auth, user) => {
+  console.log(auth);
+  console.log(auth.currentUser);
+  console.log(user);
+  return new Promise((resolve, reject) => {
+    console.log(user.email + "," + user.password);
+    const credential = EmailAuthProvider.credential(user.email, user.password);
+    console.log(credential);
+    console.log(auth.currentUser.uid);
+    resolve(auth.currentUser.uid);
+    /* 인증정보가 다른 사용자 계정에 이미 연결되어 있다면 아래 코드 에러 발생함
+    linkWithCredential(auth.currentUser, credential)
+      .then((usercred) => {
+        console.log(usercred);
+        const user = usercred.user;
+        console.log("Account linking success", user.uid);
+        resolve(user.uid);
+      })
+      .catch((e) => reject(e));
+    */
+  });
+};
+
+export const signupEmail = (auth, user) => {
+  return new Promise((resolve, reject) => {
+    createUserWithEmailAndPassword(auth, user.email, user.password)
+      .then((userCredential) => {
+        sendEmail(userCredential.user).then(() => {
+          resolve(userCredential.user.uid);
+        });
+      })
+      .catch((e) => reject(e));
+  });
+};
+
+export const sendEmail = (user) => {
+  return new Promise((resolve, reject) => {
+    sendEmailVerification(user)
+      .then(() => {
+        resolve("해당 이메일에서 인증메세지를 확인 후 다시 로그인 해주세요.");
+      })
+      .catch((e) => reject(e + ": 인증 메일 오류입니다."));
   });
 };
 
