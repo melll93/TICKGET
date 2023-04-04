c9d6ab3d3b0652bb48f1ab07899fbac089d20782
hun0711 <sh6351@naver.com>
2023-04-04 23:18:15 +0900

    마켓게시판수정

--- final_front/src/axios/market/marketLogic.js
+++ final_front/src/axios/market/marketLogic.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 //마켓 게시판 게시글 조회 로직
-export const mk_boardListDB = (board) => {
+export const mk_boardListDB = async () => {
+  const result = await axios({
+    method : "GET",
+    url : "http://localhost:8888/market/mk_boardList",
+
+  }).then((res) => res.data);
+  console.log(result);
+  return result;
+}
+/* export const mk_boardListDB = (board) => {
   console.log(board);
   return new Promise((resolve, reject) => {
     try {
@@ -15,58 +24,41 @@ export const mk_boardListDB = (board) => {
       reject(error);
     }
   });
-};
+}; */
+
 
 //마켓 게시판 글쓰기 등록 로직
-export const mk_boardInsertDB = (board) => {
-  console.log(board);
-  return new Promise((resolve, reject) => {
-    try {
-      const response = axios({
-        method: "post",
-        url: "http://localhost:8888/market/mk_boardInsert",
-        data: board,
-      });
-      resolve(response);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+export const mk_boardInsertDB = async () => {
+  const result = await axios({
+    method: "POST",
+    url: "http://localhost:8888/market/mk_boardInsert",
+
+  }).then((res) => res.data);
+  console.log(result);
+  return result;
+}
 
 //마켓 게시판 글 수정 로직
-export const mk_boardUpdateDB = (board) => {
-  console.log(board);
-  return new Promise((resolve, reject) => {
-    try {
-      const response = axios({
-        method: "post",
-        url: "http://localhost:8888/market/mk_boardUpdate",
-        data: board,
-      });
-      resolve(response);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+export const mk_boardUpdateDB = async () => {
+  const result = await axios({
+    method:"POST",
+    url: "http://localhost:8888/market/mk_boardUpdate",
+  
+  }).then((res) => res.data);
+  console.log(result);
+  return result;
+}
 
 //마켓 게시판 글 삭제 로직
-export const mk_boardDeleteDB = (board) => {
-  console.log(board);
-  return new Promise((resolve, reject) => {
-    try {
-      const response = axios({
-        method: "get",
-        url: "http://localhost:8888/market/mk_boardDelete",
-        params: board,
-      });
-      resolve(response);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+export const mk_boardDeleteDB = async () => {
+  const result = await axios({
+    method:"GET",
+    url: "http://localhost:8888/market/mk_boardDelete",
+  
+  }).then((res) => res.data);
+  console.log(result);
+  return result;
+}
 
 // 이미지 구현
 export const uploadImageDB = (file) => {
--- final_front/src/pages/market/MarketRow.jsx
+++ final_front/src/pages/market/MarketRow.jsx
@@ -1,12 +1,13 @@
   import React from 'react'
+import { Link } from 'react-router-dom';
 
   const MarketRow = ({boards}) => {
-    console.log(boards); // 도네이션 게시판 조회 데이터
+    console.log(boards); // 마켓 게시판 조회 데이터
     return (
       <>
       <tr>
         <td style={{textAlign:"center"}}>{boards.boardMkNo}</td> {/* 게시글 번호 */} 
-        <td style={{textAlign:"center"}}>{boards.boardMkTitle}</td> {/* 게시글 제목 */} 
+        <td style={{textAlign:"center"}}><Link to={"/market/detail/"+boards.boardMkNo}>{boards.boardMkTitle}</Link></td> {/* 게시글 제목 */} 
         <td style={{textAlign:"center"}}>{boards.mkTicketCount}</td> {/* 판매 티켓 수량*/} 
         <td style={{textAlign:"center"}}>{boards.mkTicketPrice}</td> {/* 판매등록가 */} 
         <td style={{textAlign:"center"}}>{boards.boardMkDate}</td>  {/* 게시글 등록일 */} 
