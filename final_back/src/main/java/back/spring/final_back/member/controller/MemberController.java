f45fada74ac7252bff99241306436641d94989a6
melll93 <yemeltu@naver.com>
2023-04-09 18:59:15 +0900

    feat. localLogin BE

---
+++ final_back/src/main/java/back/spring/final_back/member/controller/MemberController.java
@@ -0,0 +1,29 @@
+package back.spring.final_back.member.controller;
+
+import back.spring.final_back.member.repository.MemberDto;
+import back.spring.final_back.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin("http://localhost:3333")
+@Controller
+@ResponseBody
+@RequiredArgsConstructor
+@RequestMapping("/member")
+@Slf4j
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/login/local")
+    public Object LocalMemberLogin(@RequestBody MemberDto memberDto) {
+        Object result = null;
+        log.info(memberDto.toString());
+        result = memberService.localMemberLogin(memberDto);
+        log.info(result.toString());
+        return result;
+    }
+}
