fdd70f1b8afaf73bfb552bc0efccdc97f11fb7fb
Kojonghoon <dui7272@gmail.com>
2023-04-09 18:07:10 +0900

    Design. board

--- final_front/src/pages/board/BoardFileInsert.jsx
+++ final_front/src/pages/board/BoardFileInsert.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const BoardFileInsert = ({files}) => {
+const BoardFileInsert = ({ files }) => {
   console.log(files);
   return (
-    <div style={{display:'block', border:'1px solid lightGray', borderRadius:'10px', minHeight:'60px', padding:'5px'}}>
-      <div style={{textAlign:"left", padding: "2px 5px 2px 5px"}}>첨부사진</div>
-      {
-        files.map((item, index)=>(
-          <div type='text' id='fileUpload' style={{padding:"5px"}} key={index}>{item}</div>
-        ))
-      }
+    <div
+      style={{
+        display: "block",
+        border: "1px solid lightGray",
+        borderRadius: "10px",
+        minHeight: "60px",
+        padding: "5px",
+      }}
+    >
+      <div style={{ textAlign: "left", padding: "2px 5px 2px 5px" }}>
+        첨부사진
+      </div>
+      {files.map((item, index) => (
+        <div type="text" id="fileUpload" style={{ padding: "5px" }} key={index}>
+          {item}
+        </div>
+      ))}
     </div>
   );
 };
--- final_front/src/pages/board/MyFilter.jsx
+++ final_front/src/pages/board/MyFilter.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const MyFilter = ({types, type, id, title, handleTitle}) => {
-  console.log(id);//qna_type
+const MyFilter = ({ types, type, id, title, handleTitle }) => {
+  console.log(id); //qna_type
   const navigate = useNavigate();
-  const location = useLocation()
+  const location = useLocation();
   const setPath = (oldItem, newItem, key) => {
-    console.log(location.pathname)
-    console.log(oldItem)
-    console.log(newItem)
-    console.log(key)
-    let path='?qna_type=' + newItem;
+    console.log(location.pathname);
+    console.log(oldItem);
+    console.log(newItem);
+    console.log(key);
+    let path = "?qna_type=" + newItem;
     return path;
-  }
+  };
   return (
-    <DropdownButton variant="" title={title} style={{border: '1px solid lightgray', borderRadius:'5px', height:'38px'}}>
-      {
-        types.map((element, index)=>(
-          <Dropdown.Item as="button" key={index} onClick={()=>{
-            if(type){
-              navigate(setPath(title,element,id));
+    <DropdownButton
+      variant=""
+      title={title}
+      style={{
+        border: "1px solid lightgray",
+        borderRadius: "5px",
+        height: "38px",
+      }}
+    >
+      {types.map((element, index) => (
+        <Dropdown.Item
+          as="button"
+          key={index}
+          onClick={() => {
+            if (type) {
+              navigate(setPath(title, element, id));
             }
             handleTitle(element);
-          }}>
-            {element}
-          </Dropdown.Item>
-        ))
-      }
+          }}
+        >
+          {element}
+        </Dropdown.Item>
+      ))}
     </DropdownButton>
   );
 };
