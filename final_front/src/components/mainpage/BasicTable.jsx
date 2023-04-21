5abd1301a71d5808ba367dcd0e5177edbe733b49
melll93 <yemeltu@naver.com>
2023-04-21 18:46:30 +0900

    log 정리

--- final_front/src/components/mainpage/BasicTable.jsx
+++ final_front/src/components/mainpage/BasicTable.jsx
@@ -11,29 +11,31 @@ import { Link } from "react-router-dom";
 }; */
 
 
-const BasicTable =({ items }) => {
-  console.log('베이직테이직'+items)
+const BasicTable = ({ items }) => {
+  // console.log('베이직테이직'+items)
   return (
     <Table hover>
-    <tbody>
-    {items.slice(0,5).map((item) => (
-      <tr key={item.no}> {/* 각 행에 고유한 key prop을 지정해주는 것이 좋습니다. */}
-        <td>
-          {item.url ===""?<img src="./images_key/WOONGS.jpg" style={{ width: "40px", height: "40px", borderRadius: "50%" }}
-          ></img> : <img src={item.url} style={{ width: "40px", height: "40px", borderRadius: "50%" }}
-          ></img>
+      <tbody>
+        {items.slice(0, 5).map((item, index) => (
+          <tr key={index}> {/* 각 행에 고유한 key prop을 지정해주는 것이 좋습니다. */}
+            <td>
+              {item.url === "" ? <img src="./images_key/WOONGS.jpg" style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+              ></img> : <img src={item.url} style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+              ></img>
 
-          }
-          
-        </td>
-          <Link to={`${item.link}${item.no}`} style={{textDecoration:'none', color:'black'}}>
-        <td colSpan={2}>{item.title}</td>
-        </Link>
-        <td>{item.detail}</td>
-      </tr>
-    ))}
-  </tbody>
-</Table>
+              }
+
+            </td>
+            <div>
+              <Link to={`${item.link}${item.no}`} style={{ textDecoration: 'none', color: 'black' }}>
+                <td colSpan={2}>{item.title}</td>
+              </Link>
+            </div>
+            <td>{item.detail}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
