f55a7972ea732e7893a21fcb1c00c7dcb285d6d1
eunyoungko0529 <eunyoungko0529@gmail.com>
2023-05-05 21:26:05 +0900

    FEAT. SEARCH festival market and carpool

--- final_front/src/axios/festival/search/search.js
+++ final_front/src/axios/festival/search/search.js
@@ -11,3 +11,18 @@ export const searchFetivalListDB = async (keyword) => {
   return result;
 };
 
+export const searchCarpoolListDB = async (keyword) => {
+  const result = await axios({
+    method: "get",
+    url: `http://localhost:8888/search/searchCarpool?keyword=${keyword}`,
+  }).then((res) => res.data);
+  return result;
+};
+
+export const searchMarketListDB = async (keyword) => {
+  const result = await axios({
+    method: "get",
+    url: `http://localhost:8888/search/searchMarket?keyword=${keyword}`,
+  }).then((res) => res.data);
+  return result;
+};
--- final_front/src/pages/menu/SearchResultPage.jsx
+++ final_front/src/pages/menu/SearchResultPage.jsx
@@ -1,61 +1,124 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
-import { searchFetivalListDB } from '../../axios/festival/search/search';
+import { searchCarpoolListDB, searchFetivalListDB, searchMarketListDB } from '../../axios/festival/search/search';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import CommonPagination from '../../components/mainpage/CommonPagination';
+import Footer from '../../components/Footer';
 
 const SearchResultPage = () => {
 
 const {keyword} = useParams();
 console.log(keyword);
 const [searchResults, setSearchResults] = useState([]);
+const [searchResults2, setSearchResults2] = useState([]);
+const [searchResults3, setSearchResults3] = useState([]);
 
-/* useEffect(() => {
-    const searchFestivals = async () => {
-      const res = await searchFetivalListDB({ keyword });
-      setSearchResults(res);
-      console.log(res);
-    };
-    searchFestivals();
-  }, [keyword]); */
+ 
+  const [page, setPage] = useState(1);   
+  const [perPage] = useState(5);  
+
+  const indexOfLastPost = page * perPage;
+  const indexOfFirstPost = indexOfLastPost - perPage;
+
+  const currentFest = (searchResults) => {
+    let currentFest = 0;
+    currentFest = searchResults.slice(indexOfFirstPost, indexOfLastPost);
+    return currentFest;
+  };
 
 
 
   useEffect(() => {
     async function areaList() {
         const data = await searchFetivalListDB(keyword);
-      console.log(data);
+        const data2 = await searchCarpoolListDB(keyword);
+        const data3 = await searchMarketListDB(keyword);
+  /*     console.log(data);
+      console.log(data2); */
+      console.log(data3);
       setSearchResults(data);
+      setSearchResults2(data2);
+      setSearchResults3(data3);
+      
     }
     areaList();
   }, [keyword]);
 
 
     return (
-        <div>
+        <>
             <Header />
             <Sidebar />
-            <div className='center'style={{margin:'80px'}}>
-            
-            <div>
-      <h2>요청 검색어 :  "{keyword}"</h2>
+            <div className='center' style={{margin:'40px'}}>
+      <h2 style={{textAlign:'center', margin:'30px'}}>요청 검색어 :  "{keyword}"</h2>
+            <div style={{marginLeft:'40px', display:'flex'}}>
+            {/* 페스티벌 */}
+            <div style={{borderRight:'1px dotted gray', flex:'1'}}>
       <ul>
       {searchResults && searchResults.length > 0 ? (
-  <ul>
-    {searchResults.map((item,i) => (
-     <li key={i}><img src={item.festMImg} style={{width:'50px', borderRadius:'5px', marginBottom:'10px'}}/><Link to={`http://localhost:3333/productsDetail/${item.festMId}`}> {item.festMName}</Link>- {item.festMLoc}</li>
+  <><ul>
+    {currentFest(searchResults).map((item,i) => (
+     <li key={i}><img src={item.festMImg} style={{width:'50px', height:'70px', borderRadius:'5px', marginBottom:'10px'}}/><Link to={`http://localhost:3333/productsDetail/${item.festMId}`}> {item.festMName}</Link>- {item.festMLoc}</li>
     ))}
-  </ul>
+  </ul>  <CommonPagination pagination={setPage} perPage={perPage} totalItems={searchResults.length}/></>
 ) : (
   <h2>검색 결과가 없습니다.</h2>
 )}
       </ul>
     </div>
+            {/* 페스티벌 */}
 
+            {/* 카풀/마켓게시판 */}
+<div style={{borderLeft:'1px dotted gray', flex:'1'}}> 
+
+{/* 카풀 */}
+<div style={{borderBottom:'1px dotted gray'}}>
+            <ul>
+             "카풀게시판"
+      {searchResults2 && searchResults2.length > 0 ? (
+  <ul>
+    {searchResults2.map((item,i) => (
+     <li key={i}> 
+<Link to={`http://localhost:3333/carpool/carpoolDetail/${item.boardCpNo}`}> {item.boardCpTitle}</Link>- {item.boardCpMemId}
+</li>
+      ))}
+  </ul>
+) : (
+    <h2>검색 결과가 없습니다.</h2>
+    )}
+      </ul>
+      </div>
+      {/* 카풀 */}
+
+      {/* 마켓 */}
+      <ul>
+             "마켓게시판"
+      {searchResults3 && searchResults3.length > 0 ? (
+  <ul>
+    {searchResults3.map((item,i) => (
+     <li key={i}> 
+<Link to={`http://localhost:3333/market/mk_boardDetail?no=${item.boardMkNo}`}> {item.boardMkTitle}</Link>- {item.mkTicketSeat}
+</li>
+      ))}
+  </ul>
+) : (
+    <h2>검색 결과가 없습니다.</h2>
+    )}
+      </ul>
+
+
+
+
+
+      
+    </div>
+            {/* 카풀/마켓게시판 */}
 
             </div>
-        </div>
+            </div>{/* center */}
+        </>
     )
 }
 
